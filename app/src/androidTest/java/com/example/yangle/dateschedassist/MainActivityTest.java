7d60a708c9ed22c5c2e096cfa9580a13d76763fc
Yang Le <yangle0125@qq.com>
2015-07-10 23:32:36 +0800

    add test case file

--- app/src/androidTest/java/com/example/yangle/dateschedassist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/yangle/dateschedassist/MainActivityTest.java
@@ -2,6 +2,7 @@ package com.example.yangle.dateschedassist;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
 
 public class MainActivityTest extends ApplicationTestCase<Application> {
 
@@ -9,7 +10,23 @@ public class MainActivityTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testPreconditions() {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @SmallTest
+    public void testDummy() throws Exception {
+        assertEquals(1, 0);
+    }
+
+    @SmallTest
+    public void testPreconditions() throws Exception {
 
     }
 }
