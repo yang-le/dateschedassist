200524e1c23667efa5bd8fe8f7bd2e23a36378ab
Yang Le <yangle0125@qq.com>
2015-07-10 23:22:54 +0800

    add test case file

---
+++ app/src/androidTest/java/com/example/yangle/dateschedassist/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.yangle.dateschedassist;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class MainActivityTest extends ApplicationTestCase<Application> {
+
+    public MainActivityTest() {
+        super(Application.class);
+    }
+
+    public void testPreconditions() {
+
+    }
+}
